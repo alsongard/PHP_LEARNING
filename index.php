5b21c19070371f12eac33edb903288d4df72a88b
alsongard <alsongard@gmail.com>
2024-05-29 08:09:56 -0400

    conditions

--- index.php
+++ index.php
@@ -44,6 +44,67 @@
 	<p>Arithmetic operators</p>
 	<p> % returns remainer</p>
 
+	<hr>
+	<!-----------CONDITIONS--------------------->
+	<h2>Control flow Statements</h2>
+	<h3>Conditions</h3>
+	<?php
+		$bool = true;
+		if ($bool){
+			echo "The value of bool is $bool";
+		}
+		echo "<br>";
 
+		//if else returns 1 action if true and else returns if false
+		$a = 4;
+		$b = 6;
+		if ($a > $b){
+			echo "Value of a : $a is greater than value of b : $b";
+		}
+		else{
+			echo "Value of b : $b is greater than value of a : $a";
+		}
+		echo "<br>";
+
+		//when having multiple conditions, use else if
+		$grade = "A";
+		if ($grade === "A"){
+			echo "You have passed with an $grade";
+		}
+		else if ($grade === "B"){
+			echo "You have passed with a $grade";
+		}
+		else if ($grade === "C"){
+			echo "You have passed with a $grade";
+		}
+		else{
+			echo "You have not yet understood.";
+		}
+		//switch statement
+		echo "<br>";
+		echo "<h3>Switch Statements</h3>";
+		$val = 1;
+		switch($val){
+			case 1:
+				echo "The value of a is 1";
+				break;
+			case 2:
+				echo "The value of a is 2";
+				break;
+			default:
+				echo "Value out of range";
+		}
+
+		//USING MATCH
+		echo "<br>";
+		echo "<h3>Match Statements</h3>";
+		$magic_number = 3;
+		$result = match($magic_number){
+			32 => "Value is 32",
+			23, 0, 30, 10 => "Value is not magic number $magic_number",
+			default => "No magic number was found"
+		};
+		echo $result;
+	?>
 </body>
 </html>
