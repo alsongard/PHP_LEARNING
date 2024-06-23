d8420a1417fa594265c51e6668153240b4492f28
alsongard <alsongard@gmail.com>
2024-06-23 08:51:33 -0700

    database connections succeeded

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,13 +3,15 @@
 #data source name (dsn)
 
 $dsn = "mysql: host=localhost; dbname=myfirstdatabase";
-$dpuser = "root";
+$dbuser = "root";
 $dbpasswd = "";
 
 try {
+    #php data objects more flexible 
+    #mysqli good for data bases
     $pdo = new PDO($dsn, $dbuser, $dbpasswd);
     $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-catch (PDOException $e){
+catch (PDOException $e){ # used to capture error message
     echo "Connection failed " . $e->getMesssage();
 }
