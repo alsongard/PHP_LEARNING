bd26e97c4d05c389a8ffaaf9df7158103b7af1fe
alsongard <alsongard@gmail.com>
2024-07-23 16:53:09 +0300

    form data submission completed

--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -1,14 +1,38 @@
 <?php
-//data source name
-$dsn = "mysql:host=localhost;dbname=myfirstdatabase";
-$dbuser = "root";
-$dbpasswd = "";
 
 
-try{
-    $pdo = new PDO($dsn,$dbuser, $dbpasswd );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $username = $_POST["username"];
+    $userPasswd = $_POST["pwd"];
+    $email  = $_POST["email"];
+
+    try{
+        require_once "./dbh.inc.php";
+        //capture data
+        // $query = "INSERT INTO userdata(username, email, pwd) VALUES (?, ?, ?) ;"; 
+        $query = "INSERT INTO userdata(username, email, pwd) VALUES (:username,:email,:pwd);";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":pwd", $userPasswd);
+        $stmt->bindParam(":email", $email);
+
+        $stmt->execute();
+        //connect database & prepared statement
+        // $stmt = $pdo->prepare($query);
+        // $stmt->execute([$username, $email, $userPasswd]);
+
+        $pdo = null;
+        $stmt = null;
+        header("Location: ../index.php");
+
+    }
+    catch(PDOException  $e)
+    {
+        die("Query failed : ".  $e);
+    }
 }
-catch(PDOException $e){
-    echo "Connection Failed : " . $e->getMessage();
+else
+{
+    header("Location: ../index.php");
 }
