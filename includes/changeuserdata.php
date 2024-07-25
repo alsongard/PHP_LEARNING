953093f5ba68f326c83870875612390712c7668b
alsongard <alsongard@gmail.com>
2024-07-25 03:48:10 +0300

    updating user details bit

---
+++ includes/changeuserdata.php
@@ -0,0 +1,39 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $username = $_POST["username"];
+    $userpasswd = $_POST["pwd"];
+    $useremail = $_POST["email"];
+
+    try
+    {
+        require_once "./dbh.inc.php";
+        //update data for the user
+        $query = "UPDATE userdata SET username = :usrname, email= :usremail, pwd = :usrpwd WHERE id = 4;";
+        
+        //prepare the statement
+        $stmt = $pdo->prepare($query);
+
+        $stmt->bindParam(":usrname", $username);
+        $stmt->bindParam(":usremail", $useremail);
+        $stmt->bindParam(":usrpwd", $userpasswd);
+
+        $stmt->execute();
+
+
+        $pdo = null;
+        $stmt = null;
+        header("Location: ../index.php");
+        exit();
+    }
+    catch(PDOException $e)
+    {
+        die("Query failed : ". $e->getMessage());
+    }
+}
+else
+{
+    header("Location: ../index.php");
+    exit(); 
+}   
--- index.php
+++ index.php
@@ -6,12 +6,22 @@
 	<title>Document</title>
 </head>
 <body>
-	<h3>Sighnup</h3>
 	<form  action="./includes/formhandler.inc.php" method="POST" >
+		<h1>Sighnup</h1>
 		<input type="text" name="username" placeholder="Enter username..">
 		<input type="password" name="pwd" placeholder="Enter password..">
 		<input type="text" name="email" placeHolder="email">
 		<input type="submit" value="submit">
 	</form>
+
+	<hr>
+    <!-----------change user data---------------->
+    <form action="./includes/changeuserdata.php" method="POST">
+		<h1>Change User Details</h1>
+		<input type="text" name="username" placeholder="Enter username..">
+		<input type="password" name="pwd" placeholder="Enter password..">
+		<input type="text" name="email" placeHolder="email">
+		<input type="submit" value="submit">
+    </form>
 </body>
 </html>
