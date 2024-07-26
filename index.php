7f3190f96818f66a91eb33c05f61ea00b51064e4
alsongard <alsongard@gmail.com>
2024-07-26 16:08:28 +0300

    form view change, submission but all working

--- index.php
+++ index.php
@@ -3,11 +3,23 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./css/form_user.css">
 	<title>Document</title>
 </head>
 <body>
-	<form  action="./includes/formhandler.inc.php" method="POST" >
-		<h1>Sighnup</h1>
+	<!-----------OPENING FORMS DEPENDING ON USER------------------->
+	<p>Click either buttons to :</p>
+	<div class="action_doer">
+		<button id="signup_btn">SIGNUP</button>
+		<button id="change_btn"  >CHANGE USER </button>
+		<button id="delete_btn">DELETE ACCOUNT</button>
+	</div>
+
+
+	<hr>
+
+	<form id="signup_form" action="./includes/formhandler.inc.php" method="POST" >
+		<h1>Signup</h1>
 		<input type="text" name="username" placeholder="Enter username..">
 		<input type="password" name="pwd" placeholder="Enter password..">
 		<input type="text" name="email" placeHolder="email">
@@ -16,21 +28,25 @@
 
 	<hr>
     <!-----------change user data---------------->
-    <form action="./includes/changeuserdata.php" method="POST">
+    <form id="change_form" action="./includes/changeuserdata.php" method="POST">
 		<h1>Change User Details</h1>
 		<input type="text" name="username" placeholder="Enter username..">
 		<input type="password" name="pwd" placeholder="Enter password..">
-		<input type="text" name="email" placeHolder="email">
+		<input type="text" name="email" placeHolder="change email...">
 		<input type="submit" value="submit">
     </form>
 
 	<hr>
 
 	<!---------------delete user data---------------->
-	<form action="./includes/deleteuser.php" method="POST">
+	<form id="delete_form" action="./includes/deleteuser.php" method="POST">
 		<h1>Delete User Details</h1>
 		<input type="text" name="username" placeholder="Enter username..">
 		<input type="password" name="pwd" placeholder="Enter password..">
 		<input type="submit" value="submit">
-</body>
+
+	</form>
+	<hr>
+	<script src="./js/logins.js" text="text/javascript"></script>
+	</body>
 </html>
---
+++ js/logins.js
@@ -0,0 +1,32 @@
+let signup_btn = document.getElementById('signup_btn');
+let change_btn = document.getElementById('change_btn');
+let delete_btn = document.getElementById('delete_btn');
+
+let submit = document.querySelector('input[type="submit"]');
+let signup_form = document.getElementById('signup_form');
+let change_form = document.getElementById('change_form');
+let delete_form = document.getElementById('delete_form');
+
+
+function remove_form(){
+    submit.addEventListener("click", ()=>{
+        signup_form.style.display = 'none';
+        change_form.style.display = 'none';
+        delete_form.style.display = 'none';
+    })
+}
+signup_btn.addEventListener('click',()=>{
+    signup_form.style.display = 'block';
+    remove_form();
+}
+)
+
+change_btn.addEventListener("click", ()=>{
+    change_form.style.display = 'block';
+    remove_form();
+})
+
+delete_btn.addEventListener("click", ()=>{
+    delete_form.style.display = 'block';
+    remove_form();
+})
