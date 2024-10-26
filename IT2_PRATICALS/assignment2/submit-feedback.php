256a52525517ffb597c4fbeb2e24fb051ee93777
alsongard <alsongard@gmail.com>
2024-10-26 04:30:20 -0400

    submission php script file working and functional

--- IT2_PRATICALS/assignment2/submit-feedback.php
+++ IT2_PRATICALS/assignment2/submit-feedback.php
@@ -1,7 +1,8 @@
 <?php
 
+    // creating a connection using MySQLI Object Oriented
     $conn = new mysqli("localhost", "root","", "campaign_feedback");
-    if ($conn -> connect_errno){
+    if ($conn->connect_errno){
         echo ("Connection failed : ".$conn->connect_error);
         die();
     }
@@ -11,14 +12,22 @@
     $candidateFeedBack = $_POST["feedback"];
     $candidateRank = $_POST["rank"];
 
+
+    if (empty($candidataName) || empty($candidateFeedBack) || empty($candidateRank) || empty($userEmail))
+    {
+        echo '<p class="erro">Enter all fields</p>';
+        die();
+    }
+
     if ($_SERVER["REQUEST_METHOD"] === "POST")
     {
-        $stmt = $conn->prepare("INSERT INTO feedback (name, email, feedback, rating) VALUES (?, ?, ?, ?);");
+        $stmt = $conn->prepare("INSERT INTO feedback (fname, email, feedback, rating) VALUES (?, ?, ?, ?);");
 
         $stmt->bind_param("sssi",$candidataName, $userEmail, $candidataName, $candidateRank);
+        $stmt->execute();
         echo "<p>Records successfully added</p>";
     }
     else{
-        header:"Location: feedback_form.html";
+        header("Location: feedback_form.html");
         die();
     }
